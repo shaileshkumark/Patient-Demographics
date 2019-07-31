887a7025bff04f7c1d6084386236418d4fbeee05
shaileshkumark <51506284+shaileshkumark@users.noreply.github.com>
2019-07-31 11:55:22 +0530

    Add files via upload

--- server/models/patient-demographics.js
+++ server/models/patient-demographics.js
@@ -5,18 +5,17 @@ const marital = {
 };
 
 module.exports = function (PatientDemographics) {
+
   /**
    * Getting patient demographics details by their id
    */
   PatientDemographics.getDemographicsById = async (id) => {
     try {
-  
+
       let loopback = require('loopback');
       let Patients = loopback.getModel('Patients');
       let patientDetailsResult = await Patients.find({ where: { PatientId: id } });
       let demographicsResult = await PatientDemographics.find({ where: { PatientId: id } });
-      // console.log('demographics==>', demographicsResult[0].Dob);
-      // console.log('patientDetails==>', patientDetailsResult[0].Email);
       let demographics = demographicsResult.map(demographics => (
         {
           dateOfBirth: demographics.Dob,
@@ -34,7 +33,7 @@ module.exports = function (PatientDemographics) {
           }
         }
       ));
-      let patientDetails = patientDetailsResult.map(patientDetails => (
+      let patientDetail = patientDetailsResult.map(patientDetails => (
         {
           id: patientDetails.PatientId,
           email: patientDetails.Email,
@@ -47,20 +46,90 @@ module.exports = function (PatientDemographics) {
             by: patientDetails.ModifyBy,
             on: patientDetails.ModifyDate
           },
-          demographics: {...demographics[0]}
+          demographics: { ...demographics[0] }
         }
       ));
 
-      if (patientDetails.length == 0) {
+      if (patientDetail.length == 0) {
         throw new Error(`Patient id doesn't Exists!`);
       }
       else {
         return {
-          patientDetails: patientDetails
+          patientDetails: patientDetail
         };
       }
     }
     catch (err) {
+      console.log('error=>', err);
+      return {
+        error: true,
+        message: err.message
+      }
+    }
+
+  }
+  /**
+   * Get all patients demographics
+   */
+
+  PatientDemographics.GetDemographics = async () => {
+    try {
+
+      let loopback = require('loopback');
+      let Patients = loopback.getModel('Patients');
+      let patientDetailsResult = await Patients.find();
+      let demographicsResult = await PatientDemographics.find();
+
+      let patientDetails = patientDetailsResult.map(patientDetails => (
+        {
+          id: patientDetails.PatientId,
+          email: patientDetails.Email,
+          name: {
+            first: patientDetails.FirstName,
+            last: patientDetails.LastName
+          },
+          createOn: patientDetails.CreateDate,
+          modify: {
+            by: patientDetails.ModifyBy,
+            on: patientDetails.ModifyDate
+          }
+        }));
+
+      //var result = patientDetails;
+      //console.log('result=>', result);
+
+      let demographics = demographicsResult.map(demographics => (
+        {
+          id: demographics.PatientId,
+          dateOfBirth: demographics.Dob,
+          age: demographics.Age,
+          gender: demographics.Gender,
+          maritalStatus: marital[demographics.MaritalStatus],
+          bloodGroup: demographics.BloodGroup,
+          created: {
+            by: demographics.CreateBy,
+            on: demographics.CreateDate
+          },
+          modify: {
+            by: demographics.ModifyBy,
+            on: demographics.ModifyDate
+          }
+        }
+      ));
+     patientDetails.map(patient => {patient.demographics = demographics.find(demo => demo.id == patient.id)});
+
+
+
+      if (patientDetails.length == 0) {
+        throw new Error(`Patients doesn't Exists!`);
+      }
+      else {
+
+        return patientDetails;
+      };
+    }
+    catch (err) {
+      console.log('err=>', err)
       return {
         error: true,
         message: err.message
